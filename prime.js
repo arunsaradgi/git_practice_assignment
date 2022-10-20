f0b23d9d7349dffcf2124c9572111280d5bc6497
Arun Saradgi <saradgi07@gmail.com>
2022-10-20 13:42:36 +0530

    adding code

---
+++ prime.js
@@ -0,0 +1,13 @@
+let count = 0;
+let num = 23;
+
+for (let i = 2; i <= num / 2; i++) {
+  if (num % 1 == 0) {
+    count++;
+  }
+}
+if (count == 0) {
+  console.log("pasdfvasdfvsvsa");
+} else {
+  console.log("asdfasdvfasdfv");
+}
